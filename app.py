b9529aa4af7232ba120621ac471b2f6584b87586
Juan Espinosa Osorio <juankespinoso@gmail.com>
2023-07-14 17:56:47 -0500

    Funciona el aplicativo con su respectivo login

---
+++ app.py
@@ -0,0 +1,157 @@
+import os
+import psycopg2
+from flask import Flask, render_template, url_for, redirect
+from dotenv import load_dotenv
+from flask_login import UserMixin, login_user, LoginManager, login_required, logout_user, current_user
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField
+from wtforms.validators import InputRequired, Length, ValidationError
+from flask_bcrypt import Bcrypt
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'thisisasecretkey'
+bcrypt = Bcrypt(app)
+load_dotenv()
+url = os.getenv("DATABASE_URL")
+connection = psycopg2.connect(url)
+
+login_manager = LoginManager()
+login_manager.init_app(app)
+login_manager.login_view = 'login'
+
+# creating the user class
+
+
+# Devuelve el usuario que está loggeado
+
+class User(UserMixin):
+    pass
+
+
+@login_manager.user_loader
+def load_user(user_id):
+    with connection:
+        with connection.cursor() as cursor:
+            cursor.execute(
+                """select id from usuarios where id = %s;""",
+                (user_id,)
+            )
+            user = User()
+            user.id = str(cursor.fetchone()[0])
+            return user
+
+
+class RegisterForm(FlaskForm):
+    username = StringField(validators=[InputRequired(), Length(
+        min=4, max=20)], render_kw={"placeholder": "Nombre de usuario"})
+
+    password = PasswordField(validators=[InputRequired(), Length(
+        min=6, max=20)], render_kw={"placeholder": "Contraseña"})
+
+    tipo_usuario = StringField(validators=[InputRequired(), Length(
+        min=0, max=20)], render_kw={"placeholder": "Tipo de usuario"})
+
+    submit = SubmitField('Register')
+
+    def validar_usuario(self, username):
+        with connection:
+            with connection.cursor() as cursor:
+                cursor.execute(
+                    """select %s = ANY(select username from usuarios) as existe;""", (username,))
+                existing_user_username = cursor.fetchone()[0]
+                if existing_user_username:
+                    raise ValidationError(
+                        'El usuario ya existe, escoja otro nombre de usuario')
+        # return existing_user_username
+
+
+class LoginForm(FlaskForm):
+    username = StringField(validators=[
+                           InputRequired(), Length(min=4, max=20)], render_kw={"placeholder": "Nombre de usuario"})
+
+    password = PasswordField(validators=[
+                             InputRequired(), Length(min=6, max=20)], render_kw={"placeholder": "Contraseña"})
+
+    submit = SubmitField('Login')
+
+
+@app.route('/', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        with connection:
+            with connection.cursor() as cursor:
+                cursor.execute(
+                    """select %s = ANY(select username from usuarios) as existe;""", (form.username.data,))
+                user = cursor.fetchone()[0]
+                if user:
+                    cursor.execute(
+                        """select md5(%s)=password is_active from usuarios where username = %s;""",
+                        (form.password.data, form.username.data,)
+                    )
+                    coincidir_password = cursor.fetchone()[0]
+                    if coincidir_password:
+                        cursor.execute(
+                            """select id::text from usuarios where username =%s;""", (form.username.data,))
+                        id_user = cursor.fetchone()[0]
+                        usuario = User()
+                        usuario.id = str(id_user)
+                        login_user(usuario)
+                        return redirect(url_for('ofertas'))
+
+    return render_template('login.html', form=form)
+
+
+@app.route('/inmobiliarias', methods=['GET', 'POST'])
+@login_required
+def ofertas():
+    return render_template('index.html')
+
+
+@app.route('/logout', methods=['GET', 'POST'])
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for('login'))
+
+
+@app.route('/signup', methods=['GET', 'POST'])
+def signup():
+    form = RegisterForm()
+    if form.validate_on_submit():
+        with connection:
+            with connection.cursor() as cursor:
+                cursor.execute(
+                    """insert into 
+                            usuarios(username, password, tipo_usuario) 
+                            values(%s, md5(%s), %s);""",
+                    (form.username.data, form.password.data, form.tipo_usuario.data)
+                )
+                return redirect(url_for('login'))
+
+    return render_template('signup.html', form=form)
+
+
+# Backend
+
+
+# Consultas
+ESTADISTICA = """SELECT tipo_inmue, AVG(valor_pedi), MAX(valor_pedi), MIN(valor_pedi)
+FROM ofertas_cali
+WHERE comuna ilike '17' and estado ilike 'Nuevo' and tipo_ofert ilike 'Venta'
+GROUP BY tipo_inmue;"""
+# Terminan consultas
+
+
+@app.get("/api/estadistica")
+def get_estadistica():
+    with connection:
+        with connection.cursor() as cursor:
+            cursor.execute(ESTADISTICA)
+            tipo_inmueble = cursor.fetchone()
+            promedio = cursor.fetchone()[1]
+    return {"tipo_inmueble": tipo_inmueble, "promedio": promedio}
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
---
+++ static/src/js/mapa.js
@@ -0,0 +1,179 @@
+// declarar variables de mapa base street
+
+var mymap = L.map("mapid").setView([3.42, -76.5221987], 12);
+
+var gray = L.tileLayer(
+  "http://{s}.tiles.wmflabs.org/bw-mapnik/{z}/{x}/{y}.png",
+  {
+    maxZoom: 18,
+    attribution:
+      '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
+  }
+);
+
+var street = L.tileLayer("http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
+  maxZoom: 19,
+  attribution:
+    '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
+});
+
+var basemaps = {
+  Grayscale: gray,
+  Streets: street,
+};
+
+// declarar variables de cada capa
+
+const servidorGeoserver = "http://localhost:9080/geoserver/";
+
+var wms_educacion = L.tileLayer.wms(`${servidorGeoserver}ofertas_cali/wms`, {
+  layers: "ofertas_cali:educacion",
+  format: "image/png",
+  transparent: true,
+});
+
+var wms_cultura = L.tileLayer.wms(`${servidorGeoserver}ofertas_cali/wms`, {
+  layers: "ofertas_cali:cultura",
+  format: "image/png",
+  transparent: true,
+});
+
+var wms_recreacion = L.tileLayer.wms(`${servidorGeoserver}ofertas_cali/wms`, {
+  layers: "ofertas_cali:recreacion",
+  format: "image/png",
+  transparent: true,
+});
+
+var wms_salud = L.tileLayer.wms(`${servidorGeoserver}ofertas_cali/wms`, {
+  layers: "ofertas_cali:salud",
+  format: "image/png",
+  transparent: true,
+});
+
+var wms_culto = L.tileLayer.wms(`${servidorGeoserver}ofertas_cali/wms`, {
+  layers: "ofertas_cali:culto",
+  format: "image/png",
+  transparent: true,
+});
+
+var wms_comunas = L.tileLayer.wms(`${servidorGeoserver}ofertas_cali/wms`, {
+  layers: "ofertas_cali:comunas",
+  format: "image/png",
+  transparent: true,
+});
+
+var wms_deporte = L.tileLayer.wms(`${servidorGeoserver}ofertas_cali/wms`, {
+  layers: "ofertas_cali:deporte",
+  format: "image/png",
+  transparent: true,
+});
+
+var wms_estaciones_mio = L.tileLayer.wms(
+  `${servidorGeoserver}ofertas_cali/wms`,
+  {
+    layers: "ofertas_cali:estaciones_mio",
+    format: "image/png",
+    transparent: true,
+  }
+);
+
+var wms_ofertas_cali = L.tileLayer.wms(`${servidorGeoserver}ofertas_cali/wms`, {
+  layers: "ofertas_cali:ofertas_cali",
+  format: "image/png",
+  transparent: true,
+});
+
+var wms_perimetro_urbano = L.tileLayer.wms(
+  `${servidorGeoserver}ofertas_cali/wms`,
+  {
+    layers: "ofertas_cali:perimetro_urbano",
+    format: "image/png",
+    transparent: true,
+  }
+);
+
+var wms_seguridad = L.tileLayer.wms(`${servidorGeoserver}ofertas_cali/wms`, {
+  layers: "ofertas_cali:seguridad",
+  format: "image/png",
+  transparent: true,
+});
+
+var wms_terrenos = L.tileLayer.wms(`${servidorGeoserver}ofertas_cali/wms`, {
+  layers: "ofertas_cali:terrenos",
+  format: "image/png",
+  transparent: true,
+});
+
+var wms_vias = L.tileLayer.wms(`${servidorGeoserver}ofertas_cali/wms`, {
+  layers: "ofertas_cali:vias",
+  format: "image/png",
+  transparent: true,
+});
+
+//var contenido = L.control.layers() //crea un mapa de contenido o control de layers
+//contenido.addOverlay(wms_educacion,"Capa educacion");
+//contenido.addOverlay(wms_cultura,"Capa cultura");
+//contenido.addOverlay(wms_recreacion,"Capa recreacion");
+//contenido.addOverlay(wms_salud,"Capa salud");
+//contenido.addOverlay(wms_culto,"Capa culto");
+//contenido.addOverlay(wms_comuna,"Capa comuna");
+//contenido.addOverlay(wms_deporte,"Capa deporte");
+//contenido.addOverlay(wms_estaciones_mio,"Capa estaciones mio");
+//contenido.addOverlay(wms_ofertas_cali,"Capa ofertas inmobiliarias");
+//contenido.addOverlay(wms_perimetro_urbano,"Capa perimetro urbano");
+//contenido.addOverlay(wms_seguridad,"Capa seguridad");
+//contenido.addOverlay(wms_terrenos,"Capa terrenos");
+//contenido.addOverlay(wms_vias,"Capa vias");
+//contenido.addOverlay(marker,"Marcador");
+
+// crear el grupo de capas
+
+var groupedOverlays = {
+  "Capas Base": {
+    "Capa terrenos": wms_terrenos,
+    "Capa vias": wms_vias,
+    "Capa comuna": wms_comunas,
+    "Capa perimetro urbano": wms_perimetro_urbano,
+  },
+
+  "Capas Tematicas": {
+    "Capa educacion": wms_educacion,
+    "Capa cultura": wms_cultura,
+    "Capa recreacion": wms_recreacion,
+    "Capa salud": wms_salud,
+    "Capa culto": wms_culto,
+    "Capa deporte": wms_deporte,
+    "Capa seguridad": wms_seguridad,
+    "Capa estaciones mio": wms_estaciones_mio,
+    "Capa ofertas inmobiliarias": wms_ofertas_cali,
+  },
+};
+
+//boton de prender capas
+L.control.groupedLayers(basemaps, groupedOverlays).addTo(mymap);
+
+// Agregar escala grafica con tamaño de 200 pixeles
+L.control
+  .scale({
+    imperial: false,
+    maxWidth: 200,
+  })
+  .addTo(mymap);
+
+//llamar capas creadas
+//contenido.addTo(mymap);
+
+wms_perimetro_urbano.addTo(mymap);
+wms_terrenos.addTo(mymap);
+wms_educacion.addTo(mymap);
+wms_cultura.addTo(mymap);
+wms_recreacion.addTo(mymap);
+wms_salud.addTo(mymap);
+wms_culto.addTo(mymap);
+wms_deporte.addTo(mymap);
+wms_seguridad.addTo(mymap);
+wms_estaciones_mio.addTo(mymap);
+wms_ofertas_cali.addTo(mymap);
+wms_vias.addTo(mymap);
+wms_comunas.addTo(mymap);
+street.addTo(mymap);
